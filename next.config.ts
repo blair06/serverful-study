02dff50ace9e9241e94cf4e104cd1acb6c61ad02
socar-blair <blair@socar.kr>
2025-08-20 22:05:00 +0900

    build: standalone 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  output: "standalone",
+  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || "",
 };
 
 export default nextConfig;
